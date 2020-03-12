9d7ebd1a8f23a731e13cf4c9e28f88b849225b3e
rootm0s <rootm0s@protonmail.com>
2020-03-12 13:04:09 +0100

    FindAutoElevatedApplications.py

--- FindAutoElevatedApplications.py
+++ FindAutoElevatedApplications.py
@@ -1,50 +1,61 @@
 import os
 import sys
+import ctypes
 
-def manifests(payload):
-	strings = ["<autoElevate>true</autoElevate>",
-				"<autoElevate xmlns=\"http://schemas.microsoft.com/SMI/2005/WindowsSettings\">true</autoElevate>"]
-	
-	try:
-		manifest = open(os.path.join(payload), "rb").read()
-	except Exception as error:
-		return False
+class scanner:
+	""" Class to scan and parse manifests inside a binary file """
+	def __init__(self):
+		self.result = []
+		self.strings = ['level="requireAdministrator"', '<autoElevate>true</autoElevate>']
 
-	for string in strings:
-		if (str(string) in str(manifest)):
+	def search(self, path):
+		for root, dirs, files in os.walk(os.path.join(path)):
+			for name in files:
+				if os.path.join(root, name).endswith(".exe"):
+					try:
+						with open(os.path.join(root, name) , "rb") as file:
+							data = str(file.read())
+					except IOError:
+						pass
+					else:
+						if self.strings[0] and self.strings[1] in data:
+							self.result.append(os.path.join(root, name))
+				else:
+					pass
+
+		return self.result
+
+class process:
+	""" Class to create processes """
+	def create(self, path):
+		result = ctypes.windll.shell32.ShellExecuteW(None, "open", path, None, None, 1)
+		if result >= 42:
 			return True
 		else:
 			return False
 
-def main():
-	results = []
-	parsed = []
-
-	try:
-		path_input = sys.argv[1]
-	except IndexError:
-		print("Unable to search for auto elevated applications due to path is incorrect or none")
-		sys.exit()
-
-	for dir, sub, file_list in os.walk(os.path.join(path_input)):
-		for file in file_list:
-			if file.endswith(".exe"):
-				if (os.path.isfile(os.path.join(dir,file)) == True):
-					results.append(os.path.join(dir,file))
-				else:
-					pass
-			else:
-				pass
-
-	for exe in results:
-		if (manifests(exe) == True):
-			parsed.append(exe)
+	def runas(self, path):
+		result = ctypes.windll.shell32.ShellExecuteW(None, "runas", path, None, None, 1)
+		if result >= 42:
+			return True
 		else:
-			pass
-
-	print("\nResults:")
-	for path in parsed:
-		print("\t* {path}".format(path=path))
+			return False
 
 if __name__ == "__main__":
-	main()
+	try:
+		result = scanner().search(str(sys.argv[1]))
+	except Exception as error:
+		print("Unable to search for auto elevated applications due to path is incorrect or none")
+	else:
+		try:
+			if sys.argv[2] == "runas":
+				for path in result:
+					if process().runas(path):
+						print("Done - Runas - {path}".format(path=path))
+			elif sys.argv[2] == "create":
+				for path in result:
+					if process().create(path):
+						print("Done - Create - {path}".format(path=path))
+		except IndexError:
+			for path in result:
+				print(path)
